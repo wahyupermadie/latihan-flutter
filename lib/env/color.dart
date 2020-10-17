b54f0aa22499d114db28dedfff04f4930d60e75a
Agus Himawan <mr.agus.him@gitlab.com>
2020-10-17 16:22:12 +0700

    Add Env Color

---
+++ lib/env/color.dart
@@ -0,0 +1,7 @@
+import 'dart:ui' show Color;
+import 'package:flutter/material.dart';
+
+// Hacktoberfest 2020 😂//
+  static final blue = Color(0xFF00AADE);
+  static final green = Color(0xFF009D57);
+  static final lightGreen = Color(0xFF90EE90);
